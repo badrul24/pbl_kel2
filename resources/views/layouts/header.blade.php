f7427ecea0f950c0f81c5e04b30028157bb2a6a0
Muhammad Khairul Iqbal <muhammakhairul22@gmail.com>
2025-05-17 12:05:36 +0700

    Delete resources directory

--- resources/views/layouts/header.blade.php
+++
@@ -1,6 +0,0 @@
-<header class="bg-white rounded-2xl shadow-lg p-6 mb-8 flex flex-col md:flex-row md:justify-between md:items-center border border-gray-100">
-    <div>
-        <h1 class="text-2xl font-bold text-gray-800">@yield('header-title', 'Welcome back, Admin!')</h1>
-        <p class="text-gray-600">@yield('header-subtitle', 'Here\'s what\'s happening with your archives today.')</p>
-    </div>
-</header>
